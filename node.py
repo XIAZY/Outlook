87190931259a6d9250e1951613844b88816deaea
Xia Zhongyang <i@mechanus.io>
2018-03-04 17:08:34 -0500

    add left and right children setter and getter in node

--- node.py
+++ node.py
@@ -1,5 +1,23 @@
 class Node:
-    def __init__(self, lo, hi):
+
+    def __init__(self, lo, hi, task_name):
         self.lo = lo
         self.hi = hi
-        
+        self.task_name = task_name
+        # the highest hi in the whole subtree
+        self.max = hi
+        # left and right children
+        self.left = None
+        self.right = None
+
+    def set_left(self, left_node):
+        self.left = left_node
+
+    def get_left(self):
+        return self.left
+
+    def set_right(self, right_node):
+        self.right = right_node
+
+    def get_right(self):
+        return self.right
