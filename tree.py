99af0ff533eab9677577d3377d856f2f7e49d86b
Xia Zhongyang <i@mechanus.io>
2018-03-04 18:14:29 -0500

    add insert for tree

--- tree.py
+++ tree.py
@@ -1,7 +1,29 @@
 from node import Node
 
 class Tree:
-    # AVL tree implemantation
+    # essentially an AVL tree implemantation
     def __init__(self, root):
         self.root = root
+    
+    def insert(self, new_node):
+        node = self.root
+        while (node is not None):
+            if (node == new_node):
+                node.set_task = new_node.get_task()
+                return node
+            else:
+                if new_node < node:
+                    left = node.get_left()
+                    if left == None:
+                        node.set_left(new_node)
+                        return node.get_left
+                    else:
+                        node = left
+                elif new_node > node:
+                    right = node.get_left()
+                    if right == None:
+                        node.set_right(new_node)
+                        return node.get_right
+                    else:
+                        node = right
         
